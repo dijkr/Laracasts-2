ef4549b7372cb69e6827e7bdb74160289329a6ba
dijkr <vandijkr@pm.me>
2022-12-01 22:07:39 +0100

    second commit

--- Collection.php
+++ Collection.php
@@ -2,5 +2,46 @@
 
 class Collection
 {
-    protected
+    protected array $items;
+
+    public function __construct(array $items)
+    {
+        $this->items = $items;
+    }
+
+    public function sum($key)
+    {
+        return array_sum(array_map(fn($item) => $item->$key, $this->items)); {
+        }
+    }
 }
+
+class Video
+{
+    public $title;
+    public $length;
+
+    public function __construct($title, $length)
+    {
+        $this->title = $title;
+        $this->length = $length;
+    }
+}
+
+class VideosCollection extends Collection
+{
+    public function length()
+    {
+        return $this->sum('length');
+    }
+}
+
+
+    $videos = new VideosCollection([
+    new Video('Deviator', 48),
+    new Video('Thowie', 116),
+    new Video('Prifbam', 98),
+]);
+// echo $collection->sum('length');
+
+echo $videos->length();
