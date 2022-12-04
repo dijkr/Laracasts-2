80428f057b395f60ffef5fac8dbadbbe75a1ec5c
dijkr <vandijkr@pm.me>
2022-12-04 01:09:31 +0100

    Exceptions

---
+++ Value-Objects/Exceptions-1.php
@@ -0,0 +1,28 @@
+<?php
+
+class User
+{
+    public function download(Video $video)
+    {
+        if (! $this->subscribed())
+        {
+            throw new Exception(('You must be subscribed to download videos.'))
+        }
+    }
+
+    public function subscribed()
+    {
+        return false;
+    }
+}
+
+class UserDownloadController
+{
+    public function show()
+    {
+        try
+        {
+            (new User)->download(new Video);
+        }
+    }
+}
---
+++ Value-Objects/Exceptions-2.php
@@ -0,0 +1,65 @@
+<?php
+
+class TeamException extends Exception
+{
+    public static function fromTooManyMembers()
+    {
+        return new static('You may not add more than 3 team members.');
+    }
+}
+
+class Member
+{
+    public $name;
+
+    /**
+     * @param $name
+     */
+    public function __construct($name)
+    {
+        $this->name = $name;
+    }
+}
+
+class Team
+{
+    protected $members = [];
+
+    public function add(Member $member)
+    {
+        if (count($this->members) === 3)
+        {
+            throw TeamException::fromTooManyMembers();
+        }
+        $this->members[] = $member;
+    }
+
+    public function members()
+    {
+        return $this->members;
+    }
+}
+
+class TeamMembersController
+{
+    public function store()
+    {
+        $team = new Team; //has a maximum of three members
+
+        try
+        {
+
+            $team->add(new Member('Jane Doe'));
+            $team->add(new Member('John Doe'));
+            $team->add(new Member('Frank Doe'));
+            $team->add(new Member('Susan Doe'));
+
+            var_dump($team->members());
+        } catch (TeamException $e) {
+            var_dump($e);
+        }
+
+    }
+}
+
+(new TeamMembersController())->store();
